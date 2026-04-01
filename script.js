b7a98ad78f896070be94154977ce1bb35dd7b480
Divyam-Nailwal <nailwaldivyam@gmail.com>
2026-04-01 17:31:12 +0530

    fetch

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 async function fetchData(){
-  const data = await fetch('https://itunes.apple.com/search?term=arijit+singh&entity=musicArtist&limit=12')
+  const data = await fetch('https://itunes.apple.com/search?term=arijit+singh&entity=song&limit=12')
   const result = await data.json()
   console.log(result)
+
+  console.log(result.results[0].artworkUrl100)
+  // result.
 }
 
 fetchData()
+
+// function check(){
+//   let data = fetchData();
+//   console.log(data.results[0]);
+// }
+
+// check()
