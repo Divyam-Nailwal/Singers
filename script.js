a762df6c4449ed2afe228508180aad41b976f578
Divyam-Nailwal <nailwaldivyam@gmail.com>
2026-04-03 02:04:58 +0530

    almost done nigga

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-async function fetchApi() {
+const search = document.querySelector("#searching");
+
+search.addEventListener("change", (e) => {
+  let searchResult = "";
+  // console.log(searchResult)
+  searchResult = e.target.value;
+  fetchApi(searchResult)
+});
+
+
+async function fetchApi(searchResult) {
   try {
-    const data = await fetch("https://itunes.apple.com/search?term=arijit+singh&entity=song&limit=12");
+    const data = await fetch(`https://itunes.apple.com/search?term=${searchResult}&entity=song&limit=12`);
     const result = await data.json();
     displaySongs(result.results);
     console.log(result.results)
@@ -21,7 +31,9 @@ function displaySongs(songs) {
         <p>${x.artistName}</p>
       </div>
     `
-  ).join("");
+  ).join("")
 }
+
+
 
 fetchApi();
