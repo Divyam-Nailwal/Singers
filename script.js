8caf5b8de1776b34d55e902ce7d80b8eaf8b7fca
Divyam-Nailwal <nailwaldivyam@gmail.com>
2026-04-03 00:33:18 +0530

    dom_liiitle bit done yoooooooo

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-async function fetchData(){
-  const data = await fetch('https://itunes.apple.com/search?term=arijit+singh&entity=song&limit=12')
-  const result = await data.json()
-  console.log(result)
-
-  console.log(result.results[0].artworkUrl100)
-  // result.
+async function fetchApi() {
+  try {
+    const data = await fetch("https://itunes.apple.com/search?term=arijit+singh&entity=song&limit=12");
+    const result = await data.json();
+    displaySongs(result.results);
+    console.log(result.results)
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
-fetchData()
+function displaySongs(songs) {
+  let cards = document.querySelector("#songs");
+  if (!cards) return;
 
-// function check(){
-//   let data = fetchData();
-//   console.log(data.results[0]);
-// }
+  cards.innerHTML = songs.map((x) => 
+    `
+      <div class="song">
+        <img src="${x.artworkUrl100}" alt="Song Image">
+        <h3>${x.trackName}</h3>
+        <p>${x.artistName}</p>
+      </div>
+    `
+  ).join("");
+}
 
-// check()
+fetchApi();
